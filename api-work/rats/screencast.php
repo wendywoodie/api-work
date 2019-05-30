8dd19b29322d38d030c4cb5e31f7b53e60b4ee7f
wendywoodie <51194071+wendywoodie@users.noreply.github.com>
2019-05-30 12:26:22 +0300

    Add files via upload

---
+++ api-work/rats/screencast.php
@@ -0,0 +1,32 @@
+<?php
+
+<body>  
+<form method="post">  
+Enter First Number:  
+<input type="number" name="number1" /><br><br>  
+Enter Second Number:  
+<input type="number" name="number2" /><br><br>  
+<input  type="submit" name="submit" value="Add">  
+</form>   
+    if(isset($_POST['submit']))  
+    {  
+        $a = $_POST['number1'];  
+        $b = $_POST['number2'];  
+        $sum =  $a+$b;     
+
+
+     $a = getValueA();
+     $b = getValueB();
+
+if ($a,$b) {
+	if (($a,$b==!"-","")
+
+} else {
+	return '"  " is not a number'
+  }
+  $to_words=$sum
+}
+echo "expected output - ".$sum; 
+echo "expected output- $";  
+
+?>php
